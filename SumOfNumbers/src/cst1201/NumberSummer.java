af3407fdc9d78835f1d63e2e024a097748d1325d
class <class@CST_N417_09.cst.citytech.cuny.edu>
2015-03-18 17:38:58 -0400

    Sum of number

--- SumOfNumbers/src/cst1201/NumberSummer.java
+++ SumOfNumbers/src/cst1201/NumberSummer.java
@@ -1,6 +1,9 @@
 package cst1201;
 
+import java.util.Scanner;
+
 public class NumberSummer {
+    private static int number;
 
     public static void main(String[] args) {
         /*
@@ -9,6 +12,28 @@ public class NumberSummer {
         up to the number entered. For example, if the user enters 50, the loop 
         will find the sum of 1, 2, 3, 4, ... 50.
          */
+        
+        // ask user for a positive nonzero integer value
+        System.out.print("Pleasr enter a positive non zero integer: ");
+        Scanner scan = new Scanner(System.in);
+        int maxNumber = scan.nextInt();
+        
+        if (maxNumber > 0) {
+            int sum = 0;
+            int numberToAdd = 1;
+            
+            while (numberToAdd <= maxNumber) {
+                System.out.print(numberToAdd++ + ", ");
+                //sum += maxNumber;
+            }
+            
+           System.out.println();
+            System.out.println( " The sume is : " + sum);
+            
+        }
+        else {
+            System.out.println("Failure, please try again." );
+        }
     }
 
 }
