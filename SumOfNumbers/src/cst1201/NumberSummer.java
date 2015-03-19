976c918c765b7eda0311666e484afa71414dfad3
JL <JL@JL>
2015-03-19 15:18:27 -0400

    Finish Sum Of Number

--- SumOfNumbers/src/cst1201/NumberSummer.java
+++ SumOfNumbers/src/cst1201/NumberSummer.java
@@ -1,5 +1,5 @@
 package cst1201;
-
+import java.util.Scanner;
 public class NumberSummer {
 
     public static void main(String[] args) {
@@ -9,6 +9,20 @@ public class NumberSummer {
         up to the number entered. For example, if the user enters 50, the loop 
         will find the sum of 1, 2, 3, 4, ... 50.
          */
+        Scanner keyboard=new Scanner(System.in);
+        int userinput=50;
+        int sum=0;
+      
+       if (userinput>=0)
+            while ( userinput>=0 ){
+                sum += userinput;
+                System.out.println(userinput--);}
+       System.out.println(sum);
+                
+        
+                        
+                
+        
     }
-
+        
 }
