3ed9acd843e7c71ef0eae3c3e3a0f57254d09d5f
class <class@CST_N417_05.cst.citytech.cuny.edu>
2015-03-23 16:50:37 -0400

    sum of numbers ..

---
+++ src/sumofnumbers/SumOfNumbers.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sumofnumbers;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author class
+ */
+public class SumOfNumbers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ 
+
+int num;
+
+String input1 = JOptionPane.showInputDialog("Enter a positive nonzero integer");
+num = Integer.parseInt(input1);
+int sum = 0;
+
+while(num >= 1){
+sum += num;
+num--;
+}
+JOptionPane.showMessageDialog(null, "Sum: " + sum);
+
+}
+}
+   
+
